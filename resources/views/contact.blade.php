6e785fbb4e2d42f1c9a1fade3d7070eaf1b35d70
Nuwandi Nawanjana <nuwandi.freeads@gmail.com>
2024-05-04 13:44:45 +0530

    Update contact.blade.php

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -12,7 +12,7 @@
                         <div class="page-title-content">
                             <h1>Contact</h1>
                             <ul class="breadcrumb">
-                                <li><a href="{{url('/')}}">Home</a></li>
+                                <li><a href="{{ url('/') }}">Home</a></li>
                                 <li><a href="" class="active">Contact</a></li>
                             </ul>
                         </div>
@@ -25,128 +25,136 @@
         <!--== Page Content Wrapper Start ==-->
 
         <!--== Page Content Wrapper End ==-->
-<!-- resources/views/contact.blade.php -->
-<div id="page-content-wrapper" class="p-9">
-    <div class="container">
-        <div class="row">
-            <!-- Contact Page Content Start -->
-            <div class="col-lg-12">
-                <!-- Contact Method Start -->
-                <div class="contact-method-wrap">
-                    <div class="row">
-                        <!-- Single Method Start -->
-                        <div class="col-lg-3 col-sm-6 text-center">
-                            <div class="contact-method-item">
-                                <span class="method-icon"><i class="fa fa-map-marker"></i></span>
-                                <div class="method-info">
-                                    <h3>Street Address</h3>
-                                    <p>Address : No 40 Baria Sreet 133/2 <br> NewYork City, NY, United States.</p>
+        <!-- resources/views/contact.blade.php -->
+        <div id="page-content-wrapper" class="p-9">
+            <div class="container">
+                <div class="row">
+                    <!-- Contact Page Content Start -->
+                    <div class="col-lg-12">
+                        <!-- Contact Method Start -->
+                        <div class="contact-method-wrap">
+                            <div class="row">
+                                <!-- Single Method Start -->
+                                <div class="col-lg-3 col-sm-6 text-center">
+                                    <div class="contact-method-item">
+                                        <span class="method-icon"><i class="fa fa-map-marker"></i></span>
+                                        <div class="method-info">
+                                            <h3>Street Address</h3>
+                                            <p>Address : No 40 Baria Sreet 133/2 <br> NewYork City, NY, United States.</p>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
-                        <!-- Single Method End -->
+                                <!-- Single Method End -->
 
-                        <!-- Single Method Start -->
-                        <div class="col-lg-3 col-sm-6 text-center">
-                            <div class="contact-method-item">
-                                <span class="method-icon"><i class="fa fa-phone"></i></span>
-                                <div class="method-info">
-                                    <h3>Phone Number</h3>
-                                    <a href="tel:0(1234)56789012">0(1234) 567 89012</a>
-                                    <a href="tel:0(1234)56789012">0(1323) 466 89012</a>
+                                <!-- Single Method Start -->
+                                <div class="col-lg-3 col-sm-6 text-center">
+                                    <div class="contact-method-item">
+                                        <span class="method-icon"><i class="fa fa-phone"></i></span>
+                                        <div class="method-info">
+                                            <h3>Phone Number</h3>
+                                            <a href="tel:0(1234)56789012">0(1234) 567 89012</a>
+                                            <a href="tel:0(1234)56789012">0(1323) 466 89012</a>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
-                        <!-- Single Method End -->
+                                <!-- Single Method End -->
 
-                        <!-- Single Method Start -->
-                        <div class="col-lg-3 col-sm-6 text-center">
-                            <div class="contact-method-item">
-                                <span class="method-icon"><i class="fa fa-envelope-open-o"></i></span>
-                                <div class="method-info">
-                                    <h3>Number Fax</h3>
-                                    <p>+1 323 555 1234 <br> +1 267 555 12342</p>
+                                <!-- Single Method Start -->
+                                <div class="col-lg-3 col-sm-6 text-center">
+                                    <div class="contact-method-item">
+                                        <span class="method-icon"><i class="fa fa-envelope-open-o"></i></span>
+                                        <div class="method-info">
+                                            <h3>Number Fax</h3>
+                                            <p>+1 323 555 1234 <br> +1 267 555 12342</p>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
-                        <!-- Single Method End -->
+                                <!-- Single Method End -->
 
-                        <!-- Single Method Start -->
-                        <div class="col-lg-3 col-sm-6 text-center">
-                            <div class="contact-method-item">
-                                <span class="method-icon"><i class="fa fa-envelope"></i></span>
-                                <div class="method-info">
-                                    <h3>Email Address</h3>
-                                    <a href="mailto:your@email.here">your@email.here</a>
-                                    <a href="mailto:your@email.here">your@email.here</a>
+                                <!-- Single Method Start -->
+                                <div class="col-lg-3 col-sm-6 text-center">
+                                    <div class="contact-method-item">
+                                        <span class="method-icon"><i class="fa fa-envelope"></i></span>
+                                        <div class="method-info">
+                                            <h3>Email Address</h3>
+                                            <a href="mailto:your@email.here">your@email.here</a>
+                                            <a href="mailto:your@email.here">your@email.here</a>
+                                        </div>
+                                    </div>
                                 </div>
+                                <!-- Single Method End -->
                             </div>
                         </div>
-                        <!-- Single Method End -->
+                        <!-- Contact Method End -->
                     </div>
+                    <!-- Contact Page Content End -->
                 </div>
-                <!-- Contact Method End -->
-            </div>
-            <!-- Contact Page Content End -->
-        </div>
 
-        <div class="row">
-            <!-- Contact Form Start -->
-            <div class="col-lg-9 m-auto">
-                <div class="contact-form-wrap">
-                    <h2>Request a Quote</h2>
+                <div class="row">
+                    <!-- Contact Form Start -->
+                    <div class="col-lg-9 m-auto">
+                        <div class="contact-form-wrap">
 
-                    <form id="contact-form" action="{{route('contact.form')}}" method="post">
-                        <div class="row">
-                            <div class="col-lg-6">
-                                <div class="single-input-item">
-                                    <input type="text" name="f_name" placeholder="First Name *" required/>
+                            <h2>Request a Quote</h2>
+                            <!-- Display success message -->
+                            @if (session('success'))
+                                <div class="alert alert-success">
+                                    {{ session('success') }}
                                 </div>
-                            </div>
+                            @endif
+                            <form id="contact-form" action="{{ route('contact.form') }}" method="post">
+                                @csrf
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                        <div class="single-input-item">
+                                            <input type="text" name="f_name" placeholder="First Name *" required />
+                                        </div>
+                                    </div>
 
-                            <div class="col-lg-6">
-                                <div class="single-input-item">
-                                    <input type="text" name="l_name" placeholder="Last Name *" required/>
+                                    <div class="col-lg-6">
+                                        <div class="single-input-item">
+                                            <input type="text" name="l_name" placeholder="Last Name *" required />
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
+
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                        <div class="single-input-item">
+                                            <input type="email" name="email" placeholder="Email Address *" required />
+                                        </div>
+                                    </div>
+
+                                    <div class="col-lg-6">
+                                        <div class="single-input-item">
+                                            <input type="text" name="subject" placeholder="Subject *" required />
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                        <div class="single-input-item">
+                                            <textarea name="message" id="message" cols="30" rows="6" placeholder="Message"></textarea>
+                                        </div>
+
+                                        <div class="single-input-item text-center">
+                                            <button type="submit" name="submit" class="btn-add-to-cart">Send
+                                                Meassage</button>
+                                        </div>
+
+                                        <!-- Form Notification -->
+                                        {{-- <div class="form-messege"></div> --}}
+                                    </div>
+                                </div>
+                            </form>
+
                         </div>
-
-                        <div class="row">
-                            <div class="col-lg-6">
-                                <div class="single-input-item">
-                                    <input type="email" name="email" placeholder="Email Address *" required/>
-                                </div>
-                            </div>
-
-                            <div class="col-lg-6">
-                                <div class="single-input-item">
-                                    <input type="text" name="subject" placeholder="Subject *" required/>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <div class="single-input-item">
-                                    <textarea name="message" id="message" cols="30" rows="6"
-                                              placeholder="Message"></textarea>
-                                </div>
-
-                                <div class="single-input-item text-center">
-                                    <button type="submit" name="submit" class="btn-add-to-cart">Send Meassage</button>
-                                </div>
-
-                                <!-- Form Notification -->
-                                {{-- <div class="form-messege"></div> --}}
-                            </div>
-                        </div>
-                    </form>
+                    </div>
+                    <!-- Contact Form End -->
                 </div>
             </div>
-            <!-- Contact Form End -->
         </div>
-    </div>
-</div>
 
 
     </body>
